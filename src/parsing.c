69871a3421c5950b542eb49eb224d6f65add9584
Raphael Faria <rafaria@paul-f4Br4s2.clusters.42paris.fr>
2025-03-27 13:27:04 +0100

    read_file

--- src/parsing.c
+++ src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: rafaria <rafaria@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 11:55:02 by rafaria           #+#    #+#             */
-/*   Updated: 2025/03/27 12:55:05 by rafaria          ###   ########.fr       */
+/*   Updated: 2025/03/27 13:26:34 by rafaria          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,34 @@
 
 
 
+char *read_file(char *filename)
+{
+	int		fd;
+	char	*content;
+	int		bytes_read;
 
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (perror("open"), NULL);
+	content = malloc(9999);
+	if (content == NULL)
+		return (perror("malloc"), close(fd), NULL);
+	bytes_read = read(fd, content, 9999);
+	if (bytes_read == -1)
+		return (perror("read"), free(content), close(fd), NULL);
+	content[bytes_read] = '\0';
+	close(fd);
+	return (content);
+}
 
 int parsing(int argc, char **argv, char **envp)
 {
-    
     if (argc == 1)
         printf("Error : No map selected, please choose a map.\n");
     if (argc >= 3)
         printf("Error : Only one map is accepted\n");    
-    return 0;
+    printf ("%s", read_file(argv[1]));
+        return 0;
 }
 
-int read_file(char *file)
-{
-    int fd;
-    read(fd, )
-}
 
