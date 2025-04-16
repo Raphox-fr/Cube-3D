c0fc160c3264193ae4a4746d365cad15831dd1e4
Raphael Faria <rafaria@paul-f3Ar4s8.clusters.42paris.fr>
2025-04-16 14:37:20 +0200

    final one

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: rafaria <rafaria@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 19:34:55 by rafaria           #+#    #+#             */
-/*   Updated: 2025/04/16 14:04:48 by rafaria          ###   ########.fr       */
+/*   Updated: 2025/04/16 14:28:53 by rafaria          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,13 @@ void	init_struct_map(t_struct *map)
 	map->ceiling[0] = 0;
 	map->ceiling[1] = 0;
 	map->ceiling[2] = 0;
+	init_v2(map);
+}
+
+void	init_v2(t_struct *map)
+{
+	map->err_malloc = 0;
+	map->map_dis = NULL;
 	map->first_line_after_infos = 0;
 	map->x_zero = 0;
 	map->y_zero = 0;
@@ -38,10 +45,4 @@ void	init_struct_map(t_struct *map)
 	map->error = 0;
 	map->map_only = NULL;
 	map->map_brut = NULL;
-	map->map_dis = NULL;
-}
-
-void	init_v2(t_struct *map)
-{
-	map->err_malloc = 0;
 }
